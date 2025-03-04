a36b1611736e09664d44ea84402b9962ec00dc65
Jimmy Lucero Vasquez <jim.octavio2016@gmail.com>
2025-03-04 12:46:57 -0500

    Actualizamos por segunda vez app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // Declaramos las variables y arrays
 let listaAmigos = [];
 let listaAmigosSorteados = [];
-// Función para agregar amigos
-function añadirAmigos() {
+// Función para agregar al amigo en la lista
+function agregarAmigo() {
     // Capturar el texto escrito por el usuario
     let amigoUsuario = document.querySelector("#amigo").value;
     if(amigoUsuario==''){
@@ -15,8 +15,27 @@ function añadirAmigos() {
         // En caso el texto no esté vacío, añadir al array amigos
         listaAmigos.push(amigoUsuario);
     }
+    // Acualizamos la lista amigos mostrada en la web
+    actualizarAmigo();
     // Limpiar la caja de texto y enfocar el cursor en ella
     limpiarCaja();
+}
+// Función para actualizar la lista de amigos mostrada
+function actualizarAmigo() {
+    // Obtener el elemento de la lista
+    let listaAmigosMostrada = document.querySelector('#listaAmigos');
+    // Limpiar la lista existente
+    asignarTextoElemento('#listaAmigos','');
+    // Iterar sobre el rango
+    for(let i=0; i<listaAmigos.length; i++){
+        // Agregar elementos a la lista
+            // Crear un nuevo elemento <li>
+            let lista = document.createElement('li');
+            lista.textContent = listaAmigos[i];
+            // Agregar el <li> al <ul>
+            listaAmigosMostrada.appendChild(lista);
+    }
+    return;
 }
 // Función para asignar texto a los elementos del DOM
 function asignarTextoElemento(elemento, texto) {
